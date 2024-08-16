66a79fa30ddb542e2600de097a3dde4b2ffdf988
seopmin <alstjqrhkr2018@gmail.com>
2024-08-17 08:40:40 +0900

    controller

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get } from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ */
+
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getPost(): Post {
+    return {
+      author: '곽민섭',
+      title: '민섭',
+      content: '나다나',
+      likeCount: 1234,
+      commentCount: 12341234,
+    };
+  }
+}
